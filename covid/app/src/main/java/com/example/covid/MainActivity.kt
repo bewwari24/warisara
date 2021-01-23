e99bdaac3aa705eb686be4debcb509d41a5a9d3a
bewwari24 <77846729+bewwari24@users.noreply.github.com>
2021-01-23 22:40:44 +0700

    Work

---
+++ covid/app/src/main/java/com/example/covid/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.covid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var text02: TextView = findViewById(R.id.text02)
+        text02.setText("Warisara Chajandee")
+
+        val toast = Toast.makeText(applicationContext, "Complete!!!",Toast.LENGTH_LONG)
+        toast.show()
+    }
+}
